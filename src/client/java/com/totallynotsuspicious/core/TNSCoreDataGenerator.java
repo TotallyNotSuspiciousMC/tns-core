679d5e6cdc6b1a73e03fda4101200b9cbb40fc61
TheDeathlyCow <thedeathlycow@gmail.com>
2025-07-04 00:23:01 -1000

    set up project

---
+++ src/client/java/com/totallynotsuspicious/core/TNSCoreDataGenerator.java
@@ -0,0 +1,11 @@
+package com.totallynotsuspicious.core;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TNSCoreDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/com/totallynotsuspicious/core/TNSCore.java
@@ -0,0 +1,17 @@
+package com.totallynotsuspicious.core;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TNSCore implements ModInitializer {
+	public static final String MOD_ID = "tns-core";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
