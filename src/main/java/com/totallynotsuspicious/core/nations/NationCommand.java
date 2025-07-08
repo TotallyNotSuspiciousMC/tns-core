049eb2c9685bd733ab51c114cf6430a54f2acea7
TheDeathlyCow <thedeathlycow@gmail.com>
2025-07-08 03:00:40 -1000

    nation get and home commands

--- src/main/java/com/totallynotsuspicious/core/nations/NationCommand.java
+++ src/main/java/com/totallynotsuspicious/core/nations/NationCommand.java
@@ -1,21 +1,27 @@
 package com.totallynotsuspicious.core.nations;
 
+import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.builder.RequiredArgumentBuilder;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.totallynotsuspicious.core.entity.component.PlayerNationComponent;
 import me.lucko.fabric.api.permissions.v0.Permissions;
 import net.minecraft.command.CommandRegistryAccess;
 import net.minecraft.command.argument.EntityArgumentType;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.command.TeleportCommand;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
-import org.apache.logging.log4j.core.jmx.Server;
-
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.TeleportTarget;
+import net.minecraft.world.World;
 
 import java.util.Arrays;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 
 import static net.minecraft.server.command.CommandManager.argument;
@@ -24,6 +30,10 @@ import static net.minecraft.server.command.CommandManager.literal;
 public final class NationCommand {
     private static boolean hasManagePermission(ServerCommandSource src) {
         return src.hasPermissionLevel(3) || Permissions.check(src, "tnscore.nations.manage");
+    }
+
+    private static boolean hasHomePermission(ServerCommandSource src) {
+        return src.hasPermissionLevel(2) || Permissions.check(src, "tnscore.nations.home");
     }
 
     public static void registerCommand(
@@ -58,9 +68,62 @@ public final class NationCommand {
                                                         ))
                                                 )
                                 )
+                )
+                .then(
+                        literal("get")
+                                .requires(NationCommand::hasManagePermission)
+                                .then(
+                                        argument(playerArg, EntityArgumentType.player())
+                                                .executes(ctx -> executeGet(
+                                                        ctx.getSource(),
+                                                        EntityArgumentType.getPlayer(ctx, playerArg)
+                                                ))
+                                )
+                )
+                .then(
+                        literal("home")
+                                .requires(NationCommand::hasHomePermission)
+                                .executes(ctx -> executeHome(ctx.getSource()))
                 );
 
         dispatcher.register(nation);
+    }
+
+    private static int executeHome(ServerCommandSource source) throws CommandSyntaxException {
+        ServerPlayerEntity player = source.getPlayerOrThrow();
+        Nation nation = PlayerNationComponent.get(player).getNation();
+
+        Vec3d homePos = Vec3d.ofCenter(nation.getData().spawn());
+        player.teleport(
+                source.getServer().getWorld(World.OVERWORLD),
+                homePos.x,
+                homePos.y,
+                homePos.z,
+                Set.of(),
+                0f,
+                0f,
+                true
+        );
+
+        return Command.SINGLE_SUCCESS;
+    }
+
+    private static int executeGet(ServerCommandSource source, ServerPlayerEntity player) {
+        Nation nation = PlayerNationComponent.get(player).getNation();
+
+        if (nation == Nation.NATIONLESS) {
+            source.sendFeedback(
+                    () -> Text.translatable("tnscore.commands.nation.get", player.getName(), nation),
+                    false
+            );
+        } else {
+            source.sendFeedback(
+                    () -> Text.translatable("tnscore.commands.nation.get.nationless", player.getName()),
+                    false
+            );
+        }
+
+        return nation.ordinal();
     }
 
     private static int executeReset(ServerCommandSource source, ServerPlayerEntity player) {
@@ -69,7 +132,7 @@ public final class NationCommand {
                 () -> Text.translatable("tnscore.commands.nation.reset.success", player.getName()),
                 true
         );
-        return 0;
+        return Command.SINGLE_SUCCESS;
     }
 
     private static int executeSet(ServerCommandSource source, ServerPlayerEntity player, String nationName) {
@@ -81,7 +144,7 @@ public final class NationCommand {
                 true
         );
 
-        return 0;
+        return Command.SINGLE_SUCCESS;
     }
 
     private static RequiredArgumentBuilder<ServerCommandSource, String> nationArgument(String name) {
