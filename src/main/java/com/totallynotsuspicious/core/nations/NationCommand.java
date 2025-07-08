0b8c692b3454f2d5b1ba0c9a61aee7cb63194a13
TheDeathlyCow <thedeathlycow@gmail.com>
2025-07-08 04:17:09 -1000

    feedback for claim command

--- src/main/java/com/totallynotsuspicious/core/nations/NationCommand.java
+++ src/main/java/com/totallynotsuspicious/core/nations/NationCommand.java
@@ -6,7 +6,9 @@ import com.mojang.brigadier.arguments.BoolArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.builder.RequiredArgumentBuilder;
+import com.mojang.brigadier.exceptions.CommandExceptionType;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.Dynamic2CommandExceptionType;
 import com.totallynotsuspicious.core.entity.component.PlayerNationComponent;
 import com.totallynotsuspicious.core.world.NationClaimChunkComponent;
 import me.lucko.fabric.api.permissions.v0.Permissions;
@@ -31,6 +33,9 @@ import static net.minecraft.server.command.CommandManager.argument;
 import static net.minecraft.server.command.CommandManager.literal;
 
 public final class NationCommand {
+    public static final Dynamic2CommandExceptionType UNKNOWN_PROPERTY_EXCEPTION = new Dynamic2CommandExceptionType(
+            (claimedFor, alreadyClaimed) -> Text.translatable("tnscore.commands.nation.claim.fail", claimedFor, alreadyClaimed)
+    );
     private static boolean hasManagePermission(ServerCommandSource src) {
         return src.hasPermissionLevel(3) || Permissions.check(src, "tnscore.nations.manage");
     }
@@ -117,20 +122,39 @@ public final class NationCommand {
                                         )
                                 )
                         )
+                        .then(literal("query")
+                                .then(argument(posArg, BlockPosArgumentType.blockPos())
+                                        .executes(ctx -> executeQueryClaim(
+                                                ctx.getSource(),
+                                                BlockPosArgumentType.getBlockPos(ctx, posArg)
+                                        )))
+                        )
                 );
 
         dispatcher.register(nation);
     }
 
-    private static int executeClaim(ServerCommandSource source, BlockPos pos, Nation nation, boolean force) {
+    private static int executeQueryClaim(ServerCommandSource source, BlockPos pos) {
+        return Command.SINGLE_SUCCESS;
+    }
+
+    private static int executeClaim(ServerCommandSource source, BlockPos pos, Nation nation, boolean force) throws CommandSyntaxException {
         Chunk chunk = source.getWorld().getChunk(pos);
 
         NationClaimChunkComponent component = NationClaimChunkComponent.get(chunk);
 
+        boolean success;
         if (force) {
             component.forceClaim(nation);
+            success = true;
         } else {
-            component.claim(nation);
+            success = component.claim(nation);
+        }
+
+        if (success) {
+            source.sendFeedback(() -> Text.translatable("tnscore.commands.nation.claim.success", nation.getTitle()), true);
+        } else {
+            throw UNKNOWN_PROPERTY_EXCEPTION.create(nation, component.getClaimedNation());
         }
 
         return Command.SINGLE_SUCCESS;
