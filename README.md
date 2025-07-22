be6c78e37c3ff27d08bb2ac2070a856cde0855de
TheDeathlyCow <thedeathlycow@gmail.com>
2025-07-22 02:40:04 -1000

    change license to agpl-3.0-or-later

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 This is the core mod for the Totally Not Suspicious (TNS) Minecraft server. It contains a number of server-specific features, tweaks, and more. This is a replacement of the old [Seasonal Datapack](https://github.com/TotallyNotSuspiciousMC/seasonal-data/) with the goal of packaging those changes into a single, more powerful, and more performant mod.
 
+This mod is licensed under AGPL-3.0-or-later. Users may be able to obtain a copy of this source in-game with the command `/tns about`.
+
 ## Key Features for Season 13 
 
 This is my current TODO list.
@@ -20,14 +22,14 @@ TotallyNotSuspicious Core Mod
 Copyright (C) 2025  TotallyNotSuspiciousMC
 
 This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or any later
+version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser General Public License for more details.
+GNU Affero General Public License for more details.
 
-You should have received a copy of the GNU Lesser General Public License
+You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
--- src/main/java/com/totallynotsuspicious/core/TNSCore.java
+++ src/main/java/com/totallynotsuspicious/core/TNSCore.java
@@ -1,5 +1,8 @@
 package com.totallynotsuspicious.core;
 
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.totallynotsuspicious.core.compat.TNSCorePlaceholders;
 import com.totallynotsuspicious.core.compat.TNSCoreSquareMapMarkers;
 import com.totallynotsuspicious.core.entity.TNSLootModifiers;
@@ -13,9 +16,21 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.command.CommandRegistryAccess;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.ClickEvent;
+import net.minecraft.text.ParsedSelector;
+import net.minecraft.text.Style;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.net.URI;
+
+import static net.minecraft.server.command.CommandManager.literal;
 
 public class TNSCore implements ModInitializer {
     public static final String MOD_ID = "tns-core";
@@ -32,6 +47,7 @@ public class TNSCore implements ModInitializer {
         CustomEventHandlerRegistry.initialize();
         TNSCorePlaceholders.initialize();
         CommandRegistrationCallback.EVENT.register(NationCommand::registerCommand);
+        CommandRegistrationCallback.EVENT.register(this::registerAboutCommand);
         TNSLootModifiers.initialize();
         if (FabricLoader.getInstance().isModLoaded("squaremap")) {
             ServerLifecycleEvents.SERVER_STARTED.register(new TNSCoreSquareMapMarkers());
@@ -42,5 +58,33 @@ public class TNSCore implements ModInitializer {
 
     public static Identifier id(String path) {
         return Identifier.of(MOD_ID, path);
+    }
+
+    private void registerAboutCommand(
+            CommandDispatcher<ServerCommandSource> dispatcher,
+            CommandRegistryAccess registryAccess,
+            CommandManager.RegistrationEnvironment environment
+    ) {
+        LiteralArgumentBuilder<ServerCommandSource> tns = literal("tns")
+                .then(literal("about")
+                        .executes(ctx -> {
+                            ctx.getSource().sendFeedback(
+                                    () -> Text.literal(
+                                                    "Copyright (C) 2025 TotallyNotSuspiciousMC. This server relies upon the TNS Core mod, which is free software licensed under AGPL-3.0-or-later. You may obtain a copy of the full license and corresponding source at: "
+                                            )
+                                            .append(Text.literal("https://github.com/TotallyNotSuspiciousMC/tns-core").setStyle(
+                                                    Style.EMPTY
+                                                            .withUnderline(true)
+                                                            .withColor(Formatting.BLUE)
+                                                            .withClickEvent(new ClickEvent.OpenUrl(URI.create("https://github.com/TotallyNotSuspiciousMC/tns-core")))
+                                            )),
+                                    false
+                            );
+
+                            return Command.SINGLE_SUCCESS;
+                        })
+                );
+
+        dispatcher.register(tns);
     }
 }
